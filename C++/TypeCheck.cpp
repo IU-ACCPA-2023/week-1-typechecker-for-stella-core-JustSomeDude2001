b661d8848c1c169b55e1531f906c8fe18dc39a99
y.kim <kim.yaroslavik@gmail.com>
2023-03-27 15:31:27 +0300

    Add successful typecheck message

--- C++/TypeCheck.cpp
+++ C++/TypeCheck.cpp
@@ -7,6 +7,11 @@ namespace Stella {
 
     void typecheckProgram(Program *program) {
         program->accept(new MyVisitor());
+        std::cout << "NOTE: There is a lot of debug info printed for developer's convenience. Do not pay mind to it.\n";
+        std::cout << "There is no check for existence of main method.\n";
+
+        std::cout << "TYPE CHECKING PASSED SUCCESSFULLY.\n";
     }
+
 
 } // namespace Stella
