16655b95ce75a8adefe35b0f0930e13fe5030716
y.kim <kim.yaroslavik@gmail.com>
2023-04-07 04:44:00 +0300

    Added minor commentary in TypeCheck.cpp and a success message.

--- C++/TypeCheck.cpp
+++ C++/TypeCheck.cpp
@@ -7,7 +7,14 @@ namespace Stella
 {
   void typecheckProgram(Program *program)
   {
-    program->accept(new MyVisitor());
+      /**
+       * Interesting features beyond requirements specified:
+       *
+       * Tuples of any size
+       * Inference of sum types is supported (I think)
+       */
+      program->accept(new MyVisitor());
+      std::cout << "Typechecking successful";
   }
 
 } // namespace Stella
